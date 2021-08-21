6661cd2b3c61e45c857dc0a94263632764f72371
Agustin978 <loboagus10@gmail.com>
2021-08-21 12:11:40 -0300

    Funcion para la limpieza del boofer

--- Gestor/helpers.py
+++ Gestor/helpers.py
@@ -1,1 +1,11 @@
 """Funciones ayuda"""
+
+import os
+#import platform
+
+#Funciones para limpiar el boofer
+def clear():
+    if platform.system()=="Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
