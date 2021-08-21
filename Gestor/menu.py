fbd93d8deb63c9316fb2023a501ba25829b41a7b
Agustin978 <loboagus10@gmail.com>
2021-08-21 12:12:18 -0300

    modificacion de las llamadas a las funciones

--- Gestor/menu.py
+++ Gestor/menu.py
@@ -1,19 +1,24 @@
 """Menu del programa"""
 
+import helpers
+import manager
+
 def loop():
     while True:
-    print("\nBienvenido al gestor del restaurante.")
-    opcion= input("Ingrese una opcion:\n[1]Agregar una categoria.\n[2]Agregar un plato.\n[3]Mostrar menu.\n[4]Salir del programa.\nOpcion:")
-    print("\n")
+        print("\nBienvenido al gestor del restaurante.")
+        opcion= input("Ingrese una opcion:\n[1]Agregar una categoria.\n[2]Agregar un plato.\n[3]Mostrar menu.\n[4]Salir del programa.\nOpcion:")
+        print("\n")
 
-    if opcion=="1":
-        agregar_categorias()
-    elif opcion=="2":
-        agregar_plato()
-    elif opcion=="3":
-        mostrar_menu()
-    elif opcion=="4":
-        print("Gracias por usar nuestro programa.")
-        break
-    else:
-        print("No se selecciono una opcion valida.")     
+        helpers.clear()
+
+        if opcion=="1":
+            manager.agregar_categorias()
+        elif opcion=="2":
+            pass 
+        elif opcion=="3":
+            pass
+        elif opcion=="4":
+            print("Gracias por usar nuestro programa.")
+            break
+        else:
+            print("No se selecciono una opcion valida.")     
