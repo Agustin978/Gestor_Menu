704fc6b1fadc7b44971ff4aca92b5cf39683293e
Agustin978 <loboagus10@gmail.com>
2021-08-23 01:33:19 -0300

    Funcion que al final no uso pero la comento por las dudas

--- Gestor/helpers.py
+++ Gestor/helpers.py
@@ -10,10 +10,26 @@ def clear():
         os.system("cls")
     else:
         os.system("clear")
-
+"""
 def bucle():
     conection= sqlite3.connect("restaurante.db")
     cursor= conection.cursor()
+
+
+    modifica= input("Ingrese el nombre del plato que desea modificar:\n>")
+
+    try:
+        nombre= input("Ingrese el nombre del plato:\n>")
+        precio= int(input("Ingrese el valor del plato:\n>"))
+        plato=[nombre,precio]
+        cursor.execute("UPDATE platos SET nombre={}, precio{}, WHERE nombre={}".format(plato[0], plato[1],modifica))
+    except sqlite3.ProgrammingError:
+        print("ERROR... No se encontro el plato solicitado. Intentelo nuevamente")
+    except ValueError:
+        print("Uno de los valores ingresados no es valido. Intente nuevamente")
+    finally:
+        pass
+
 
     categorias= cursor.execute("SELECT * FROM categorias")
     
@@ -35,7 +51,8 @@ def bucle():
         platos= cursor.execute("SELECT * FROM platos WHERE categoria_id={}".format(opcion))
 
         #Tengo que hacer todavia el bloque while y for para modificar algun plato de la categoria
-
+        op_2= input("Ingrese el nombre del plato que desea modificar:\n>")
+        """
 
 
 
